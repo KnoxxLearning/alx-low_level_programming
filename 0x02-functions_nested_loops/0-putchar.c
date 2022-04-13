8e385d730989c175152eca7ae3f2622406563dbf
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-13 13:55:14 -0700

    Program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - "A program that prints _putchar, followed by a new line"
+* File: "0-putchar.c"
+* Return: "Always 0 (Success)"
+*/
+
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
