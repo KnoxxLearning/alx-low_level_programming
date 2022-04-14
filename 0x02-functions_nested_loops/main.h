e7016e36f4b2de286dfeb884608bcae9e4203c49
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-13 20:55:30 -0700

    Our main.h library

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_
+/**
+* File: "main.h"
+* Auth: "Nokuthula Chirwa"
+* Desc: "This will be the header file we reference to declare the functions we need for 0x02-functions_nested_loops directory"
+*/
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _alpha(int c);
+int print_sign(int x);
+int _abs(int);
+int print_last_digit(int);
+#endif
