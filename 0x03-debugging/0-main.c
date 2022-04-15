b0a085a8bdadfbf5073fd8e54f03990e729488f2
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-14 17:15:28 -0700

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - function for testing the sign of our interger
+ * Return:  Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
