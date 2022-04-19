92028e896048dc76dfefcbbda7eeba33725d7d1f
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-19 08:37:25 -0700

    The alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
-* main –  “Entry point”
-* Return : “Always 0 (Success)
+* main – “Entry point”
+* Return : “Always 0 (Success)"
 */
 
 int main(void)
 {
 	char l;
 
-	for (l  = ‘z’ ;  l => ‘a’ ; l--)
+	for (l  = 'z' ;  l >= 'a' ; l--)
 	{
 		putchar(l);
-	}									}
-		putchar(‘\n’);
+	}
+		putchar('\n');
 		return (0);
 }
 
