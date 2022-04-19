ef2133cc9b00c28e263ca62e2c8cd606ad5ef199
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-19 15:09:55 -0700

    Random variable assignment with source code that prints the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	char str[] = "Last digit of ";
 
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
+	n = rand() - RAND_MAX / 2;
 	last = n % 10;
 	if (last > 5)
 	{
@@ -24,7 +24,7 @@ int main(void)
 	{
 		print("%s%d is %d and is 0\n", str, n, last);
 	}
-	else if (n <6)
+	else if (n < 6)
 	{
 		printf(%s%d is %d and is less than 6 and not 0\n", str, n, last);
 	}
