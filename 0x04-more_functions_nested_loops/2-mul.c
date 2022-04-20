4b5d66fcdbbec05a6f4ca5a211635e2a5968149d
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-19 20:24:46 -0700

    Function that multiplies 2 intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#incllude "main.h"
+/**
+ * mul = "Function that multiplies two intergers"
+ * @a: "1st integer to be multiplied"
+ * @b: "2ns integer to be multiplied"
+ *
+ * Return: "1 if the number is a digit, and 0 otherwise"
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
