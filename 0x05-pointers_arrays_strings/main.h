ec962999af00b4602cdd8eb20b6988b544352b46
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-20 19:13:36 -0700

    Our hearder files

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * file: main.h
+ * Desc: "The prototypes for all the functions that we'll be using for this directory are in this header files."
+ */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+#endif
