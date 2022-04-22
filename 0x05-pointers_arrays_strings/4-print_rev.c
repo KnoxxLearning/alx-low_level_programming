e3740c593be7f8b300a5622b95f1d21e706b9385
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-21 17:06:03 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,18 +4,18 @@
  * print_rev - "function used to print string in reverse"
  * @s: "the string we'll reverse"
  *
- * 
+ *
  */
 void print_rev(char *s)
 {
 	int len = 0;
-	
+
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	
-	for (len = len -1; len >= 0; len--)
+
+	for (len = len - 1; len >= 0; len--)
 	{
 		_putchar(s[len]);
 	}
