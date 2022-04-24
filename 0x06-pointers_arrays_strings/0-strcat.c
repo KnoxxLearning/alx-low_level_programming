b701203b2274f8731965135dd1a9eda21fea7a13
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-24 00:53:02 -0700

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
-include <string.h>
-include <stdio.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * _strcat - "The function to concatenate two strings"
  * @src: string to be appended
@@ -13,13 +13,11 @@ char *_strcat(char *dest, char *src);
 	int len = 0;
 	int c = 0;
 
-	while (dest[len] != 0)
-	{
+	while (dest[len] = 0) {
 		len++;
 	}
 
-	while (src[c] != 0)
-	{
+	while (src[c] != 0) {
 		dest[len] = src[c];
 		len++;
 	}
