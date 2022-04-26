f5bf771e0e2a4a2cf6391cbce244cb435cdc0e3e
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-26 03:10:32 -0700

    Function that locates a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * -strchr - "Function used to locate a string"
+ *  @c: "character to be searched for" 
+ *  @s: "Pointer to the string to be scanned"
+ *
+ *Return: "The first occurance of character c and NULL if unfound"
+ */
+char *_strchr(char *s, char c)
+{
+	int t = 0;
+	int u = 0;
+
+	while (s[t])
+	{
+		u++;
+	}
+	for (t = 0; t < u; u++)
+	{
+		if (s[t] == c)
+		{
+			return (s + x);
+		}
+	}
+	return ('\0');
+}
