a26d783d2bb70a9d06b2b828835c1f2861e28003
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-26 16:27:31 -0700

    Updates to 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,21 @@
 #include "main.h"
+
 /**
- * more_numbers - "function that prints 10 times the numbers, from 0 to 14"
+ * more _numers - "Function that prints 10x the numbers 0-14"
+ *
  */
 void more_numbers(void)
 {
-	int num, count;
+int i, j;
 
-	for (count = 0; count <= 9; count++)
-	{
-		for (num = 0; num <= 14; num++)
-		{
-			if (num > 9)
-			{
-				_putchar ((num / 10) + '0')
-			}
-			_putchar((num % 10) + '0');
-		}
-		_putchar('\n');
-	}
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+if (j >= 10)
+_putchar((j / 10) + '0');
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * more_numbers - "Function that prints 10 times the numbers 0-14"
+ *
+ *
+ *
+ 
