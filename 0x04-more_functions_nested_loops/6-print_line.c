0ed44a8ddc9b3c7df835d90e62bc198f3853a820
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-26 16:53:47 -0700

    6 updates

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,26 @@
 #include "main.h"
 
 /**
- * more_numbers - "Function that prints 10 times the numbers 0-14"
+ * print_line - "Function that draws a straight line in the terminal"
+ * @n - "Number of times the character should be printed"
  *
+ * Return: "Print '\n' if n is 0 or less"
  *
- *
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		int i;
+			for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
  
