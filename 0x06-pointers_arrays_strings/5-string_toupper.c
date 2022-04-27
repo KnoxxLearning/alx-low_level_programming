714de72142d8164f4eb4b076ede414573b2e8af7
KnoxxLearning <chirwanokuthula@gmail.com>
2022-04-27 09:41:08 -0700

    Function that turns lowercase string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - "Function that changes lowercase to uppercase"
+ * @str: "Lowercase string we'll change to uppercase"
+ *
+ * Return: "The string that's been changed to uppercase"
+ */
+char *string_toupper(char *)
+{
+	int t;
+	i = 0;
+
+	while (str[t] != '\0')
+	{
+		if (str[t] >= 97 && str[t] = 122)
+		{
+			str[t] = str[t] - 32;
+		}
+		t++;
+	}
+	return (str);
+}
